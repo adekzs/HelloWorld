026396a86b5a1ae4bd26ee1a5d3d7ce65a4e1be7
Michael Afolabi <adekuinleafolabi354@gmail.com>
2021-04-26 07:26:53 +0100

    Added Increment and Decrement functionality

--- app/src/main/java/com/adeks/helloworld/MainActivity.kt
+++ app/src/main/java/com/adeks/helloworld/MainActivity.kt
@@ -2,10 +2,24 @@ package com.adeks.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var texVal : Int = 0
+    var textView : TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.textView)
+    }
+
+    fun incrementTextView(view: View) {
+        texVal += 1
+        textView?.setText(texVal.toString())
+    }
+    fun decrementTextView(view: View) {
+        texVal -= 1
+        textView?.setText(texVal.toString())
     }
 }
